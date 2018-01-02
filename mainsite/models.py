5a2a9a850b6450e57b7a4c070483757246473b75
seiching <seiching@gmail.com>
2018-01-02 20:19:41 +0800

    add

---
+++ mainsite/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post (models.Model):
+    title = models.CharField(max_length=200) 
+    slug  = models.CharField(max_length=200)
+    pub_date =models.DateTimeField(default = timezone.now) 
+
+    body = models.TextField()
+    class Meta:
+       ordering = ('-pub_date',)
+    def  __unicode__(self):
+        return self.title
+
